931d7d129f0ae2fb3a9d6b2f114b71d30085c27d
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2024-10-29 18:56:25 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.scss'
+import Header from './components/Header/Header'
+import Hero from './components/Header/Hero/Hero'
+import Catalog from './components/Header/Catalog/Catalog'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/> 
+    <Catalog/>
+    </>
+)
+
+}
+
+export default App
